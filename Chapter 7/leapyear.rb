c3d1c07ca0961a650009caf5bd3079e11ae1b481
Rebecca Appleyard <rklappleyard@gmail.com>
2015-03-28 19:16:52 +0000

    added leap years solution

--- Chapter 7/leapyear.rb
+++ Chapter 7/leapyear.rb
@@ -3,10 +3,15 @@ start_year = gets.chomp.to_i
 
 print "End year: "
 end_year = gets.chomp.to_i
+puts ""
 
 for year in start_year..end_year
-  if year % 4 == 0
-    puts ""
+  if year % 100 == 0
+    if year % 400 == 0
+      puts year
+    end
+
+  elsif year % 4 == 0
     puts year
   end
 end
