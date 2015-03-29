e2d54f22ad1fb9936f26f0a833843e4c5066edfe
Rebecca Appleyard <rklappleyard@gmail.com>
2015-03-29 17:37:11 +0100

    Added more solutions
    My solutions to Chapter 9

---
+++ Chapter 9/old_school_roman_numerals.rb
@@ -0,0 +1,62 @@
+def old_roman_numeral(num)
+	roman_num = ""
+
+	thous = num / 1000
+	roman_num << "M" * thous
+	num = num % 1000
+
+	hund = num / 100
+	if hund == 9
+		roman_num << "CM"
+		num = num % 100
+	elsif hund == 4
+		roman_num << "CD"
+		num = num % 100
+	else
+		five_hund = num / 500
+		num = num % 500
+		roman_num << "D" * five_hund
+
+		num = num % 100
+		roman_num << "C" * hund
+	end
+
+	ten = num / 10
+	if ten == 9
+		roman_num << "XC"
+		num = num % 10
+	elsif ten == 4
+		roman_num << "XL"
+		num = num % 10
+	else
+		fifty = num / 50
+		num = num % 50
+		roman_num << "L" * fifty
+
+		ten = num / 10
+		num = num % 10
+		roman_num << "X" * ten
+	end
+
+	if num == 4
+		roman_num << "IV"
+	elsif num == 9
+		roman_num << "IX"
+	else
+		five = num / 5
+		num = num % 5
+		roman_num << "V" * five
+		roman_num << "I" * num
+	end
+puts roman_num
+end
+
+old_roman_numeral(124)
+old_roman_numeral(4)
+old_roman_numeral(9)
+old_roman_numeral(89)
+old_roman_numeral(459)
+old_roman_numeral(972)
+
+
+
